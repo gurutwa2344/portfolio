92603234fde3ee108f7338eebbf84494c78431ab
gurutwa2344 <haribhattaraibhattarai2039@gmail.com>
2024-12-29 17:04:22 +0545

    finished

---
+++ js/index.js
@@ -0,0 +1,25 @@
+
+
+// let a=21
+// let b=21
+// console.log(a+b);
+
+// if(true){
+//     console.log('ta');
+//     console.log('ta');
+//     console.log('ta');
+//     console.log('ta');
+//     console.log('ta');
+//     console.log('ta');
+//     console.log('ta');
+//     console.log('ta');
+//     console.log('ta');
+//     console.log('ta');
+//     console.log('ta');
+//     console.log('ta');
+//     console.log('ta');
+//     console.log('ta');
+    
+// }
+
+
