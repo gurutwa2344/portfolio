47d4f41d060fd6777f99b4f65a85df919e77c7ad
gurutwa2344 <haribhattaraibhattarai2039@gmail.com>
2024-12-31 17:42:22 +0545

    new files added

---
+++ js/inde.js
@@ -0,0 +1,15 @@
+let day = prompt("day")
+
+if (day >= Today && day <= Today) {
+    console.log("Mostly Sunny");
+}
+
+else if (day >= tomorrow && day <= tomorrow) {
+    console.log("Mostly sunny and not as warm");
+
+}
+
+else {
+    console.log("Today to Second Wednesday");
+
+}
--- js/index.js
+++ js/index.js
@@ -1,25 +1,49 @@
 
 
-// let a=21
-// let b=21
-// console.log(a+b);
+// let a = 21
+// let b = 21
+// console.log(a + b);
 
-// if(true){
-//     console.log('ta');
-//     console.log('ta');
-//     console.log('ta');
-//     console.log('ta');
-//     console.log('ta');
-//     console.log('ta');
-//     console.log('ta');
-//     console.log('ta');
-//     console.log('ta');
-//     console.log('ta');
-//     console.log('ta');
-//     console.log('ta');
-//     console.log('ta');
-//     console.log('ta');
-    
+// if (true) {
+//     console.log('tyape');
+//     console.log('tyape');
+//     console.log('tyape');
+//     console.log('tyape');
+//     console.log('tyape');
+//     console.log('tyape');
+//     console.log('tyape');
+//     console.log('tyape');
+//     console.log('tyape');
+//     console.log('tyape');
+//     console.log('tyape');
+//     console.log('tyape');
+//     console.log('tyape');
+//     console.log('tyape');
+
 // }
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let marks= prompt("enter your marks")
+
+if(marks >= 40 && marks <= 70){
+    console.log("pass")
+}
+
+else if(marks >70 && marks <= 90){
+    console.log("Excellent") 
+}
+
+else if(marks >90 && marks <= 100){
+    console.log("OutStanding") 
+}
+
+else if(marks > 100){
+    console.log("Marks Invalid") 
+}
+
+else{
+    console.log("fail")
+    
+}
 
